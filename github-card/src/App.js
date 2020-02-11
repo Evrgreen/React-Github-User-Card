0efa23ef9325096f3bcb98d58a85a0a1c326d3fa
Evrgreen <rcarsten06@gmail.com>
2020-02-11 15:03:56 -0700

    axios call for mainuser/followers

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import axios from "axios";
 
 
 
-function App() {
+class App extends React.Component {
+  state = {
+    user:{},
+    followers:[]
+  }
+
+  componentDidMount(){
+    axios.get(`https://api.github.com/users/evrgreen`)
+    .then(response=>{
+      console.log(response)
+      this.setState({user:response.data})
+      axios.get(`https://api.github.com/users/Evrgreen/followers`)
+      .then(response =>{
+        console.log(response)
+        this.setState({followers:response.data})
+      })
+    })
+    .catch(error=>{
+      console.log(error)
+    })
+  }
+
+
+  render(){
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>{this.state.user.login}</h1>
+      {this.state.followers.map(follower=>(
+        <p key={Date.now()}>{follower.login}</p>
+      ))}
     </div>
   );
+  }
 }
 
 export default App;
